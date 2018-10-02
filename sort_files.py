3437e2d8e1f19bc689fab3dc7bbd63be23a6999f
Ali Sultan <ajasultan@gmail.com>
2018-10-02 19:52:24 +0400

    make a list of extracted cities

--- sort_files.py
+++ sort_files.py
@@ -5,3 +5,12 @@ def extract_name(file_name):
     city_name = file_name[file_name.find("_")+1:]
     city_name = city_name[:city_name.find("_")]
     return city_name
+
+os.chdir('files')
+files = os.listdir()
+cities = []
+
+for file in files:
+    city_name = extract_name(file)
+    if city_name not in cities:
+        cities.append(city_name)
