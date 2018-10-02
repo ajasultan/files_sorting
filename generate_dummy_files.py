49ee36cd09088709c66386c7b09e0827ac2c1a81
Ali Sultan <ajasultan@gmail.com>
2018-10-02 19:21:03 +0400

    Initilizing generate_dummy_files

---
+++ generate_dummy_files.py
@@ -0,0 +1,7 @@
+import os
+import random
+
+os.mkdir('files') # make a new directory called files
+os.chdir('files') # move to files directory
+cities = ['Muscat', 'London', 'Barcelona', 'Salalah', 'Manchester', 'New York'
+          , 'Dubai']
