64770227eedebba04eed6fe192bc89da8e5b2ab0
Ali Sultan <ajasultan@gmail.com>
2018-10-04 19:24:51 +0400

    modified to follow Python style guide

--- generate_dummy_files.py
+++ generate_dummy_files.py
@@ -2,34 +2,34 @@ import os
 import random
 
 
-#Function that generates a random date for the file name
-def generate_date(month,year):
-    months_31 = [1,3,5,7,8,10,12]
+# Function that generates a random date for the file name
+def generate_date(month, year):
+    months_31 = [1, 3, 5, 7, 8, 10, 12]
     if month in months_31:
-        day = random.randrange(1,30)
+        day = random.randrange(1, 30)
     elif month == 2:
         if year % 4 == 0:
-            day = random.randrange (1,29)
+            day = random.randrange(1, 29)
         else:
-            day = random.randrange(1,28)
+            day = random.randrange(1, 28)
     else:
-        day = random.randrange(1,30)
+        day = random.randrange(1, 30)
     return day
 
 
-#Function that generates a random time for the file name
+# Function that generates a random time for the file name
 def generate_time():
-    hour = random.randrange(0,23)
+    hour = random.randrange(0, 23)
     if hour < 10:
-        hour = '0'+str(hour)
+        hour = '0' + str(hour)
     else:
         hour = str(hour)
-    minutes = random.randrange(0,59)
+    minutes = random.randrange(0, 59)
     if minutes < 10:
-        minutes = '0'+str(minutes)
+        minutes = '0' + str(minutes)
     else:
         minutes = str(minutes)
-    seconds = random.randrange(0,59)
+    seconds = random.randrange(0, 59)
     if seconds < 10:
         seconds = '0'+str(seconds)
     else:
@@ -37,20 +37,20 @@ def generate_time():
     return '_' + hour + minutes + seconds
 
 
-os.mkdir('files') # make a new directory called files
-os.chdir('files') # move to files directory
-cities = ['Muscat', 'London', 'Barcelona', 'Salalah', 'Manchester', 'New York'
-          , 'Dubai']
+os.mkdir('files')  # make a new directory called files
+os.chdir('files')  # move to files directory
+cities = ['Muscat', 'London', 'Barcelona', 'Salalah', 'Manchester', 'New York',
+          'Dubai']
 
 index = 0
 while index < 150:
     city_name = random.choice(cities)
-    year = random.randrange(2009,2017)
-    month = random.randrange(1,12)
-    day = generate_date(month,year)
-    start = str(year)+str(month)+str(day)+"_"
+    year = random.randrange(2009, 2017)
+    month = random.randrange(1, 12)
+    day = generate_date(month, year)
+    start = str(year) + str(month) + str(day) + "_"
     end = generate_time()
     file_name = start+city_name+end
-    f = open(file_name+'.txt','w+') # create a new file
-    f.close() # close created file
+    f = open(file_name + '.txt', 'w+')  # create a new file
+    f.close()  # close created file
     index += 1
--- sort_files.py
+++ sort_files.py
@@ -1,13 +1,17 @@
 import os
 
+
 # Function that extract name from a file of formate ddmmyyy_filename_hhmmss
 def extract_name(file_name):
-    city_name = file_name[file_name.find("_")+1:]
+    city_name = file_name[file_name.find("_") + 1:]
     city_name = city_name[:city_name.find("_")]
     return city_name
 
+
 os.chdir('files')
+
 files = os.listdir()
+
 cities = []
 
 for file in files:
